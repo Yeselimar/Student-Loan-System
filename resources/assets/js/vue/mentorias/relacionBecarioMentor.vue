e847b4f827b5044dd8a1208f051e7d11b7848bcd
Darwin Ceballo <darniw@gmail.com>
2018-11-03 13:04:23 -0400

    añadir sweetarlert, modificacion becario-mentor

--- resources/assets/js/vue/mentorias/relacionBecarioMentor.vue
+++ resources/assets/js/vue/mentorias/relacionBecarioMentor.vue
@@ -1,7 +1,9 @@
 <script>
+
 import vSelect from 'vue-select'
 Vue.component('v-select',vSelect);
 Vue.use(BootstrapVue);
+
 import relacionBecarioMentor from "../../components/relacionBecarioMentorComponent.vue";
 const app = new Vue({
   el: "#relacionBecarioMentorID",
